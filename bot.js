a663f1485e9ef8ac475052173a7e41b8c21b336e
MaxDrolls <49698378+MaxDrolls@users.noreply.github.com>
2019-07-25 06:06:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
     if (err) console.error(err);
   });
   credits[message.author.id] = {
-      credits: m + 99999999,
+      credits: m + 0.10,
   }
 
     if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
@@ -226,7 +226,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 2500000000;
+    let amount = 685;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
